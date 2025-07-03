bacd5e81608a7c68e730dbb9c9c09e71b3b14ec4
fars90 <andreraposeirofaria@gmail.com>
2025-07-03 11:31:16 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -35,6 +35,13 @@ def obter_resposta(texto: str) -> str:
         'o que achas do caso anjos?': 'Ridiculo!',
         'tens algum filme favorito?': 'Claro, o Matrix ;)',
         'gostas de musica?': 'Sim, musica classica!',
+        'és adepto de algum clube de futebol?': 'O grande Sporting!!!!',
+        'qual é a tua cor favorita?': 'Azul',
+        'sagres ou super-bock?': 'super-bock, obvio!',
+        'o que és': 'Sou um simples chatbot, mas com muito para dar!',
+        'estás a aprender': 'Estou sempre a aprender com as tuas perguntas!',
+        'quantos anos tens': 'Fui criado recentemente, mas envelheço muito devagar :)',
+        'fala sobre ti': 'Sou um bot simpático que adora conversar contigo!',
         ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
     }
 
